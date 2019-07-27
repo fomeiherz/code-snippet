1168d93ca4bf83ffc81e12133af3cb45479d15e3
fomeiherz <fomeiherz@users.noreply.github.com>
2019-07-27 15:10:11 +0800

    Update CatFilterConfigure.java

--- CatFilterConfigure.java
+++ CatFilterConfigure.java
@@ -4,6 +4,9 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * spring-boot新增一个启动的ServletFilter方式
+ */
 @Configuration
 public class CatFilterConfigure {
 
